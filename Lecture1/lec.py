df12574d3fccb612dc9eeacf413c9223d95c77d5
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-12-14 19:17:56 +0200

    add folder Lecture1

---
+++ Lecture1/lec.py
@@ -0,0 +1,70 @@
+#print('hello world')
+#a = 123
+#b = 1.23
+#value = None
+#s = 'Hello world'
+
+#print(a)
+#print(b)
+#print(type (a))
+#print(type (b))
+#print(type (value))
+
+#print(a,b,s)
+#print(a,'-',b,'-',s)
+#print('{2} - {1} - {0}'.format(a,b,s))
+#print(f'{a} - {b} - {s}')
+
+#f = True
+#print(f)
+
+#list=[1,2,3,'hello']
+#print(list)
+
+#print('Введите a')
+#a = int(input())
+#print('Введите b')
+#b = int(input())
+#print(a,'+',b,'=',a+b)
+#print('{} {}'.format(a,b))
+
+#a=1.3
+#a+=3
+#b=3
+#c=a/b
+#c1=a//b #целое от деления
+#c2=a%b #остаток от деления
+#с3=a**b #степень
+#c4 = round(a*b,3)
+#print(c)
+#print(c1)
+#print(c2)
+#print(c4)
+
+#a=1<4 and 5>2
+#print(a)
+
+#a=[1,2]
+#b=[1,2]
+#print(a==b)
+
+#a=2
+#b=9
+#c=7
+#print(a<b>c)
+
+#f=[1,2,3,4]
+#print(f)
+#print(2 in f) #2ка в списке
+#print(not 2 in f) #нет 2ки в списке
+
+#is_odd = f[0] % 2 == 0 #первый элемент списка четный?
+#is_odd = not f[0] % 2
+#print(is_odd)
+
+a = int(input('a = '))
+b = int(input('b = '))
+if a > b:
+    print(a)
+else:
+    print(b)
